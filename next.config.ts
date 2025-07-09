f11bab133b275e6a802a457e98e4bb14f39ab5fb
Gul7333 <g.task.pk@gmail.com>
2025-07-09 19:37:12 +0500

    fixes html verification

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,23 @@
 import type { NextConfig } from "next";
+import { Header, Rewrite } from "next/dist/lib/load-custom-routes";
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
+  rewrites: async ():Promise<Rewrite[]>=>[
+   
+    {
+      source: '/google8c405f60274f7059.html',
+      destination: '/public/google8c405f60274f7059.html'
+    }
+   
+
+  ]
+  ,
+    
+
+  
   output: "export"
   /* config options here */
 };
